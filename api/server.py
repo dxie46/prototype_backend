67689da57cba74b82f3e321c81b6a4acd17dbdb5
dxie46 <91031361+dxie46@users.noreply.github.com>
2023-07-24 14:02:35 -0400

    final

--- api/server.py
+++ api/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from PIL import Image
 import pickle
 import os
-from fastai.vision.all import *
+from fastai.vision.all import load_learner, predict, PILImage
 from utils.parse import label_func
 
 app = Flask(__name__)
@@ -18,7 +18,6 @@ def predict():
     #     train()
     
     # make prediction
-    # learn = load_learner('./transfer_learn_fastai.pkl')
-    # result = learn.predict(image)
-    # return jsonify({"result": result[0], "probability": str(result[2].numpy()[0])})
-    return "hi"
+    learn = load_learner('./transfer_learn_fastai.pkl')
+    result = learn.predict(image)
+    return jsonify({"result": result[0], "probability": str(result[2].numpy()[0])})
